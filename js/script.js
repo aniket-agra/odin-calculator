c67bf4d6f0012316b06a6ca895e30230082cbfff
Aniket Agrawal <aniket.mpa@gmail.com>
2022-09-18 13:12:59 +0200

    Add display with base value of 0

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let buttonSection = document.querySelector(".buttons");
+
+
+
+for (let i = 0; i < 10; i++) {
+    let newButton = document.createElement("button");
+    newButton.textContent = i;
+    buttonSection.append()
+}
