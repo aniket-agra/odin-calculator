0069056de488c1950930c7ff53ddfbf86602a11b
Aniket Agrawal <aniket.mpa@gmail.com>
2022-09-18 13:25:30 +0200

    Show clicked button in display

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
+let displaySection = document.querySelector(".display");
+
 let buttonSection = document.querySelector(".buttons");
-
-
 for (let i = 0; i < 4; i++) {
     let buttonRow = document.createElement("div");
     for (let j = 0; j < 4; j++) {
         let buttonCol = document.createElement("button");
-        buttonCol.addEventListener('click', function (e) {
-
-        });
         buttonCol.textContent = "#";
+        buttonCol.addEventListener('click', function (e) {
+            displaySection.textContent = buttonCol.textContent;
+        });
         buttonRow.append(buttonCol);
     }
     buttonSection.append(buttonRow);
