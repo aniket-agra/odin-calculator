0ac42cf85b17d3e789393517c0b1ed5a7304b95c
Aniket Agrawal <aniket.mpa@gmail.com>
2022-09-19 07:51:33 +0200

    Call separate function on button click

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 let displaySection = document.querySelector(".display");
 
+function clickFunction() {
+    console.log(this);
+}
+
 let buttonSection = document.querySelector(".buttons");
 for (let i = 0; i < 4; i++) {
     let buttonRow = document.createElement("div");
     for (let j = 0; j < 4; j++) {
         let buttonCol = document.createElement("button");
-        buttonCol.textContent = "#";
-        buttonCol.addEventListener('click', function (e) {
-            displaySection.textContent = buttonCol.textContent;
-        });
+        buttonCol.textContent = `${i * 4 + j}`;
+        buttonCol.addEventListener('click', clickFunction);
         buttonRow.append(buttonCol);
     }
     buttonSection.append(buttonRow);
