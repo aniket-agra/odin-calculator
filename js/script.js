e36bd0e991e2f81f3f6ebcabe17fa8fd774de042
Aniket Agrawal <aniket.mpa@gmail.com>
2022-09-20 08:56:47 +0200

    Add logic for calculator, test style for calculator

--- js/script.js
+++ js/script.js
@@ -2,10 +2,32 @@ let displaySection = document.querySelector(".display");
 
 let currentExpr = [];
 
-function evalExpr() {
-    
+function operate(operator, operand1, operand2) {
+    switch(operator) {
+        case "+" : return operand1 + operand2;
+        case "-" : return operand1 - operand2;
+        case "*" : return operand1 * operand2;
+        case "/" : return operand1 / operand2;
+        default  : return "Operation not supported";
+    }
 }
 
+function evalExpr(expression = currentExpr) {
+    // extract entries one by one from left 3 at a time and try to evaluate
+    let result = expression.shift();
+    // if evaluation fails NaN or undefined is returned
+    if (!isNaN(result) && result !== undefined) {
+        while (expression.length !== 0) {
+            let operator = expression.shift();
+            let operand = expression.shift();
+            result = operate(operator, Number(result), Number(operand));
+        }
+    }
+    else {
+        alert("Invalid Input! Please try again.")
+    }
+}
+// evalExpr(["12", "+", "7", "-", "-", "*", "3"]);
 function clickFunction() {
     // get clicked button
     let clickedButton = this.textContent;
